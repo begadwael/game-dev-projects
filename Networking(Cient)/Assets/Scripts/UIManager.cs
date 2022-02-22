9fd70a97562d3ea508cefd449df20696885e9095
begad <begad.mestar@gmail.com>
2022-02-22 22:24:47 +0400

    C# networking (not done)

---
+++ Networking(Cient)/Assets/Scripts/UIManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager instance;
+
+    public GameObject StartMenu;
+    public InputField usernameField;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != null)
+        {
+            Debug.Log("instance already exists, destorying object");
+            Destroy(this);
+        }
+    }
+
+    public void ConnectToServer()
+    {
+        StartMenu.SetActive(false);
+        usernameField.interactable = false;
+        Client.instance.ConnectToServer();
+    }
+}
---
+++ networking/GameServer/GameServer/ServerHandle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameServer
+{
+    class ServerHandle
+    {
+        public static void WelcomeReceived(int _fromClient, Packet _packet)
+        {
+            int _clintIdCheck = _packet.ReadInt();
+            string _username = _packet.ReadString();
+
+            Console.WriteLine($"{Server.clients[_fromClient].tcp.socket.Client.RemoteEndPoint}" +
+                $" connected successfully and is now player {_fromClient}.");
+
+            if (_fromClient != _clintIdCheck)
+            {
+                Console.WriteLine($"Player \"{_username}\" (ID: {_fromClient} has assumed the wrong client ID ({_clintIdCheck})!");
+            }
+        }
+
+        public static void UDPTestReceived(int _fromClient, Packet _packet)
+        {
+            string _msg = _packet.ReadString();
+
+            Console.WriteLine($"Received packet via UDP. contains message: {_msg}");
+        }
+    }
+}
